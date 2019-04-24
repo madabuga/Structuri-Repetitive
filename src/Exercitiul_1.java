8cb3fb6c24ce2370c8330395ac156372697b5ae6
Madalina Buga <mada.buga@yahoo.com>
2019-04-24 23:35:34 +0300

    Ex1

--- src/Exercitiul_1.java
+++ src/Exercitiul_1.java
@@ -1,5 +1,41 @@
+// 1. sa se scrie un program folosind structuri repetitive, care sa afiseze urmatorul rezultat:
+// #  ##  ###   ####   ###  ##  #
+
 public class Exercitiul_1 {
     public static void main(String[] args) {
-        
+
+        for (int i = 1; i <= 4; i++) {
+
+            int j=0;
+
+            while(j!=i){
+
+                System.out.print("#");
+
+                j++;
+
+            };
+
+            System.out.print("  ");
+
+            if(i==4){
+
+                for (int k = 3; k > 0; k--) {
+
+                    j=0;
+
+                    while(j!=k){
+
+                        System.out.print("#");
+
+                        j++;
+
+                    };
+
+                    System.out.print(" ");
+                }
+            }
+        }
+
     }
 }
