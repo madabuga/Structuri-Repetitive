4b525f6cc21d546c38c18d04809274f1055a37a4
Madalina Buga <mada.buga@yahoo.com>
2019-05-01 16:56:40 +0300

    Ex6

---
+++ src/Exercitiul_6.java
@@ -0,0 +1,12 @@
+/*
+6. Se da un numar natural n. Afisati in ordine crescatoare, primele n numere naturale impare
+ */
+public class Exercitiul_6 {
+    public static void main(String[] args) {
+        int n=9;
+        for (int i = 1; i < n*2; i=i+2) {
+            System.out.println(i);
+        }
+    }
+
+}
