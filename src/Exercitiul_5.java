482684e9f94b0fcd365e363b30c6aa8ae20ff47b
Madalina Buga <mada.buga@yahoo.com>
2019-05-01 16:51:10 +0300

    Ex5

---
+++ src/Exercitiul_5.java
@@ -0,0 +1,15 @@
+import javafx.scene.transform.Scale;
+
+import java.util.Scanner;
+
+/*
+5. se da un numar natural n. afisati in ordine descrescatoare, primele n numere naturale nenule.
+ */
+public class Exercitiul_5 {
+    public static void main(String[] args) {
+              int  n = 7;
+        for (int i = n; i > 0; i--) {
+            System.out.println(i);
+        }
+    }
+}
