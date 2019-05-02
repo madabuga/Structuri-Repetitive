019692639f076a0d5164d67fb27f708a3ab31476
Madalina Buga <mada.buga@yahoo.com>
2019-05-02 19:48:25 +0300

    Ex14

---
+++ src/Exercitiul_14.java
@@ -0,0 +1,13 @@
+/*
+14. scrieti o bucla infinita care va afisa de n ori "going".
+
+ */
+
+public class Exercitiul_14 {
+    public static void main(String[] args) {
+
+        for (int i = 0; ; i++) {
+            System.out.print("going ");
+        }
+    }
+}
