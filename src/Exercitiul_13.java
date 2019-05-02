42d0006766a0ebfacb0ba9edfab34a15f3c18d80
Madalina Buga <mada.buga@yahoo.com>
2019-05-02 19:45:06 +0300

    Ex13

---
+++ src/Exercitiul_13.java
@@ -0,0 +1,16 @@
+/*
+13. se da un numar n. scrieti un program care va afisa de n ori mesajul "go", urmat de gata.
+
+ */
+
+public class Exercitiul_13 {
+    public static void main(String[] args) {
+        int n=11;
+
+        for (int i = 0; i < n; i++) {
+            System.out.print("go ");
+        }
+
+        System.out.print("Gata");
+    }
+}
